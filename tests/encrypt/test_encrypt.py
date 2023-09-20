0181070920b93acd12a051c5b7624fc815cd347a
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-20 15:54:59 -0300

    🧪 test: fix encrypt message tests

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -4,14 +4,14 @@ import pytest
 
 def test_encrypt_message():
     invalid_key = "invalid"
+    valid_message = "encrypted"
     with pytest.raises(TypeError, match="tipo inválido para key"):
-        encrypt_message("message", invalid_key)
+        encrypt_message(valid_message, invalid_key)
 
     invalid_message = 7
     with pytest.raises(TypeError, match="tipo inválido para message"):
         encrypt_message(invalid_message, 5)
 
-    message = "encrypted"
-    assert encrypt_message(message, 10) == message[::-1]
-    assert encrypt_message(message, 3) == "cne_detpyr"
-    assert encrypt_message(message, 4) == "detpy_rcne"
+    assert encrypt_message(valid_message, 10) == valid_message[::-1]
+    assert encrypt_message(valid_message, 3) == "cne_detpyr"
+    assert encrypt_message(valid_message, 4) == "detpy_rcne"
