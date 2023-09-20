504cdcfe6ff51f6b6e29357c74b52975a243333a
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-20 14:07:11 -0300

    ✨ feat: req 1, study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    count = 0
+
+    for student_arrive, student_leave in permanence_period:
+        if not isinstance(student_arrive, int) or not isinstance(
+            student_leave, int
+        ):
+            return None
+        if student_arrive <= target_time <= student_leave:
+            count += 1
+
+    return count
