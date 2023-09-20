5a4889f647a99f5e6728dd5fcfc2698be3f60676
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-20 15:50:03 -0300

    🧪 test: fix req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,16 +2,16 @@ from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
 
-def test_encrypt():
+def test_encrypt_message():
     invalid_key = "invalid"
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message("message", invalid_key)
+
     invalid_message = 7
     with pytest.raises(TypeError, match="tipo inválido para message"):
-        encrypt_message(invalid_message, invalid_message)
+        encrypt_message(invalid_message, 5)
+
     message = "encrypted"
     assert encrypt_message(message, 10) == message[::-1]
-    message = "encrypted"
     assert encrypt_message(message, 3) == "cne_detpyr"
-    message = "encrypted"
     assert encrypt_message(message, 4) == "detpy_rcne"
