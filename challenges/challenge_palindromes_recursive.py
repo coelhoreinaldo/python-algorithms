5c1f0772d7bce94309a5a026445ac0f0e29ac7ed
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-20 14:32:47 -0300

    ✨ feat: req 3, palindrome recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,14 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+
+    if len(word) == 1:
+        return True
+
+    WORD = word.lower()
+
+    if low_index <= high_index:
+        if not is_palindrome_recursive(WORD, low_index + 1, high_index - 1):
+            return False
+
+    return WORD[low_index] == WORD[high_index]
