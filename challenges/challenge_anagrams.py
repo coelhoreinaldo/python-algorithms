96c3c03694478ed3bcc98d7b0db8ea40989c09ee
coelhoreinaldo <scoelhor7@gmail.com>
2023-09-20 15:13:25 -0300

    ✨ feat: req 4, anagramas

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,41 @@
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return merge(left, right, array.copy())
+
+
+def merge(left, right, merged):
+    left_cursor, right_cursor = 0, 0
+
+    while left_cursor < len(left) and right_cursor < len(right):
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor + right_cursor] = left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
+
+    return merged
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not first_string.isalpha() and not second_string.isalpha():
+        return (first_string, second_string, False)
+
+    first_sorted = merge_sort(list(first_string.lower()))
+    second_sorted = merge_sort(list(second_string.lower()))
+
+    return (
+        "".join(first_sorted),
+        "".join(second_sorted),
+        first_sorted == second_sorted,
+    )
